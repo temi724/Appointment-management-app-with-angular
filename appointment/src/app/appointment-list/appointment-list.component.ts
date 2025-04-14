d5101d432807205bda968305fe6c4c00f83fa8f1
DANIEL TEMITAYO PETER <tayodaniel0@gmail.com>
2025-04-14 17:09:46 +0100

    Adding local storage functionality to the appointment list and retrieving data from local storage with angular ngOnInit  Changes to be committed: 	modified:   appointment/src/app/app.component.ts 	modified:   appointment/src/app/appointment-list/appointment-list.component.html 	modified:   appointment/src/app/appointment-list/appointment-list.component.ts

--- appointment/src/app/appointment-list/appointment-list.component.ts
+++ appointment/src/app/appointment-list/appointment-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
@@ -7,11 +8,22 @@ import { Appointment } from '../models/appointment';
   templateUrl: './appointment-list.component.html',
   styleUrl: './appointment-list.component.css',
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
   newAppointmentTitle: string = '';
   newAppointmentDate: Date = new Date();
   appointments: Appointment[] = [];
 
+  ngOnInit(): void {
+    // Load appointments from local storage
+    const storedAppointments = localStorage.getItem('appointments');
+    if (storedAppointments) {
+      this.appointments = JSON.parse(storedAppointments);
+    } else {
+      this.appointments = [];
+    }
+  }
+
+  //Add appointment
   addAppointment() {
     if (!this.newAppointmentTitle || !this.newAppointmentDate) {
       return;
@@ -24,5 +36,12 @@ export class AppointmentListComponent {
     this.appointments.push(newAppointment);
     this.newAppointmentTitle = '';
     this.newAppointmentDate = new Date();
+    localStorage.setItem('appointments', JSON.stringify(this.appointments));
+  }
+
+  //Delete appointment
+  removeAppointment(index: number) {
+    this.appointments.splice(index, 1);
+    localStorage.setItem('appointments', JSON.stringify(this.appointments));
   }
 }
